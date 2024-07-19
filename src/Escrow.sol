49b98e5232acc2f78686d1da0521f9380aa2faa6
whitewizardd <leumasre@gmail.com>
2024-07-19 03:38:52 +0100

    escrow contract

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.26;
 
 contract Escrow {
-    address public seller;
+    address public user;
+    address public useer2;
+    uint256 public amount;
+    address[] public disputeResolver;
+    uint8 immutable escrowCommission;
+
+    constructor(address _user1, address _user2, uint256 _amount, address[] memory _resolver, uint8 commission) {
+        user = _user1;
+        user = _user2;
+        amount = _amount;
+        disputeResolver = _resolver;
+        escrowCommission = commission;
+    }
 }
